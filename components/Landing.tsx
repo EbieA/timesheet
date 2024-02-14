3fc26a3719bd4ced1eba736b3afb6e49d4c723d6
Ebie <adewumiebenezer7@gmail.com>
2024-02-14 16:34:00 +0100

    navbar and responsiveness done

---
+++ components/Landing.tsx
@@ -0,0 +1,57 @@
+// import React from "react";
+// import Image from "next/image";
+// import { Button } from "./ui/button";
+
+// const Landing = () => {
+//   return (
+//     <div>
+//       <div className="w-[1446px] h-[641px]  lg:flex-shrink sm:block relative bg-white">
+//         <div className="left-[94px] top-[97px] absolute text-blue-950 text-[50px] font-semibold font-['Nunito']">
+//           The simple way to track <br />
+//           productivity, save time <br />
+//           and increase profit
+//         </div>
+//         <div className="left-[94px] top-[316px] absolute text-stone-400 text-xl font-bold font-['Nunito']">
+//           The All-in-One Tracking Software App for Productivity
+//           <br /> increase, project management, Seamless Payment <br />
+//           Partnerships for Remote Team, Employee onboarding
+//           <br />
+//           and Management
+//         </div>
+//         <div className="w-[402px] h-6 left-[94px] top-[551px] absolute text-stone-500 text-[13px] font-normal font-['Nunito']">
+//           Enjoy all features for free ( 30 days trail - No credit card
+//           required.)
+//         </div>
+//         <Button className="w-[357px] h-16 left-[92.97px] top-[474px] absolute bg-teal-500">
+//           Start Using for FREE – Sign up here
+//         </Button>
+//         <div className="w-[852.50px] h-[474.50px] left-[557px] top-[97px] absolute">
+//           <Image
+//             className="left-0 top-0 absolute opacity-70"
+//             src="/shadow.png"
+//             width={852.5}
+//             height={474.5}
+//             alt="macbook"
+//           />
+//           <Image
+//             className="left-[14px] top-0 absolute"
+//             src="/macbook.png"
+//             width={773}
+//             height={465}
+//             alt="macbook"
+//           />
+//           <Image
+//             className="left-[256px] top-[23px] absolute"
+//             src="/dashboard.png"
+//             width={514.5}
+//             height={379.75}
+//             alt="macbook"
+//           />
+//           <div className="w-24 h-[18px] left-[447.40px] top-[396.08px] absolute origin-top-left rotate-[2.30deg] bg-black" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Landing;
---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,241 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { Fragment, useState } from "react";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { Button } from "../ui/button";
+
+const products = [
+  {
+    name: "Sub Contractor",
+    href: "#",
+  },
+  {
+    name: "HR Management",
+    href: "#",
+  },
+  {
+    name: "Enterpreneur",
+    href: "#",
+  },
+  {
+    name: "Recruiters",
+    href: "#",
+  },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Landing() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <header className="h-20 bg-white shadow items-center justify-center w-[1462px] fixed z-10">
+        <nav
+          className="flex gap-x-32 lg:justify-center sm:justify-start items-center p-6"
+          aria-label="Global"
+        >
+          <div className="flex  w-auto">
+            <Link href="/" className="flex items-center">
+              <Image alt="logo" src="/logo.png" width={38} height={38} />
+              <Image
+                className="ml-2"
+                alt="logo"
+                src="/ADMIN-TIMESHEET.svg"
+                width={184}
+                height={14}
+              />
+            </Link>
+          </div>
+          <div className="flex lg:hidden">
+            <button
+              type="button"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(true)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <Popover.Group className="hidden justify-center item-center lg:flex lg:gap-x-12">
+            <Popover className="relative">
+              <Popover.Button className="flex focus:outline-none items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+                Use Cases
+                <ChevronDownIcon
+                  className="h-5 w-5 flex-none text-gray-400"
+                  aria-hidden="true"
+                />
+              </Popover.Button>
+
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="opacity-0 translate-y-1"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in duration-150"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1"
+              >
+                <Popover.Panel className="absolute left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                  <div className="p-4">
+                    {products.map((item) => (
+                      <div
+                        key={item.name}
+                        className="group relative flex items-center gap-x-4 rounded-lg p-2 text-sm leading-4 hover:bg-gray-50"
+                      >
+                        <div className="flex-auto">
+                          <a
+                            href={item.href}
+                            className="block font-semibold text-gray-900"
+                          >
+                            {item.name}
+                            <span className="absolute inset-0" />
+                          </a>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </Popover.Panel>
+              </Transition>
+            </Popover>
+
+            <a
+              href="/pricing"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Pricing
+            </a>
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Blog
+            </a>
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Book a Demo
+            </a>
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Contact Us
+            </a>
+          </Popover.Group>
+          <div className="flex justify-center items-center">
+            <div className="hidden lg:flex space-x-4">
+              <Button className=" w-[114.74px] h-[45px] bg-teal-500 rounded-lg hover hover:bg-teal-400 active:bg-teal-300 focus:outline-none focus:ring focus:ring-teal-300">
+                Sign Up
+              </Button>
+              <Button className="w-[115.74px] h-[45px] bg-white rounded-lg border border-teal-500 text-teal-500 hover:bg-white active:bg-white focus:outline-none focus:ring focus:ring-gray-100">
+                Login
+              </Button>
+            </div>
+          </div>
+        </nav>
+
+        <Dialog
+          as="div"
+          className="lg:hidden fixed inset-0 z-50 overflow-y-auto"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+        >
+          <div className="fixed inset-0 z-10" />
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex p-6">
+              <button
+                type="button"
+                className="-m-6.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  <Disclosure as="div" className="-mx-3">
+                    {({ open }) => (
+                      <>
+                        <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                          Product
+                          <ChevronDownIcon
+                            className={classNames(
+                              open ? "rotate-180" : "",
+                              "h-5 w-5 flex-none"
+                            )}
+                            aria-hidden="true"
+                          />
+                        </Disclosure.Button>
+                        <Disclosure.Panel className="mt-2 space-y-2">
+                          {[...products].map((item) => (
+                            <Disclosure.Button
+                              key={item.name}
+                              as="a"
+                              href={item.href}
+                              className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                            >
+                              {item.name}
+                            </Disclosure.Button>
+                          ))}
+                        </Disclosure.Panel>
+                      </>
+                    )}
+                  </Disclosure>
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Use Cases
+                  </a>
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Pricing
+                  </a>
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Blog
+                  </a>
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Book a Session
+                  </a>
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Contact Us
+                  </a>
+                </div>
+                <div className="py-6">
+                  <div className="flex justify-center items-center space-x-4">
+                    <Button className=" w-[114.74px] h-[45px] bg-teal-500 rounded-lg">
+                      Sign Up
+                    </Button>
+                    <Button className="w-[115.74px] h-[45px] bg-white rounded-lg border border-teal-500 text-teal-500">
+                      Login
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Dialog.Panel>
+        </Dialog>
+      </header>
+    </>
+  );
+}
